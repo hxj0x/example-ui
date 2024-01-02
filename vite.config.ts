c89853957297ad102c796bc42261e95e963cabe9
hxj0x <2063818987@qq.com>
2024-01-02 22:35:10 +0800

    add base for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // for GitHub pages
+  base: '/example-ui',
   plugins: [react()],
   resolve: {
     // https://cn.vitejs.dev/config/#resolve-alias
